5c5c4085ed36cd064dfa8a0e9a76d76131295bf6
PAVAN KUMAR REDDY <pavankumart46@gmail.com>
2019-12-23 16:18:54 +0530

    End Of Day One FDP

--- SRKITFDPFIRST/app/src/main/java/pavankreddy/blogspot/srkitfdpfirst/MainActivity.java
+++ SRKITFDPFIRST/app/src/main/java/pavankreddy/blogspot/srkitfdpfirst/MainActivity.java
@@ -1,5 +1,6 @@
 package pavankreddy.blogspot.srkitfdpfirst;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -18,16 +19,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv = findViewById(R.id.counttv);
+
+        if(savedInstanceState!=null){
+            count = savedInstanceState.getInt("COUNT");
+            tv.setText(String.valueOf(count));
+        }
     }
 
     public void showToast(View view) {
 
-        Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
+       /* Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();*/
+        count--;
+        tv.setText(String.valueOf(count));
     }
 
     public void showCount(View view) {
         count++;
         /*tv.setText(""+count); --> this works too */
         tv.setText(String.valueOf(count));
+    }
+
+    // Step - 1 -> Override onsaveInstanceState
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Step-2 --> Decide on what to save!
+        outState.putInt("COUNT",count);
     }
 }
