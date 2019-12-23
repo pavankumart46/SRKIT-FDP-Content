ad661c49946783f10062d3f4a562fdc594452ecb
PAVAN KUMAR REDDY <pavankumart46@gmail.com>
2019-12-23 15:51:57 +0530

    DESIGN PHASE - 1 Complete

--- SRKITFDPFIRST/app/src/main/java/pavankreddy/blogspot/srkitfdpfirst/MainActivity.java
+++ SRKITFDPFIRST/app/src/main/java/pavankreddy/blogspot/srkitfdpfirst/MainActivity.java
@@ -3,12 +3,31 @@ package pavankreddy.blogspot.srkitfdpfirst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.counttv);
+    }
+
+    public void showToast(View view) {
+
+        Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void showCount(View view) {
+        count++;
+        /*tv.setText(""+count); --> this works too */
+        tv.setText(String.valueOf(count));
     }
 }
