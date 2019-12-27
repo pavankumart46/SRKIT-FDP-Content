2fa369608b7adc654e7e43dcf0158d80eb437775
PAVAN KUMAR <pavankumart46@gmail.com>
2019-12-27 12:53:10 +0530

    Movie Info added

--- MoviesInfo/app/src/main/java/com/blogspot/pavankreddy/moviesinfo/MainActivity.java
+++ MoviesInfo/app/src/main/java/com/blogspot/pavankreddy/moviesinfo/MainActivity.java
@@ -2,17 +2,20 @@ package com.blogspot.pavankreddy.moviesinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     int images[];
     String movies[],actors[];
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpData();
+        recyclerView = findViewById(R.id.recyclerview);
     }
 
     private void setUpData() {
